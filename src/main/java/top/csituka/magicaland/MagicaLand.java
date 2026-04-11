79ccf901498b4ceaf9c2fcafe3f23765334d1990
Csituka_D <694428809@qq.com>
2026-04-12 06:36:22 +0800

    微调

--- src/main/java/top/csituka/magicaland/MagicaLand.java
+++ src/main/java/top/csituka/magicaland/MagicaLand.java
@@ -2,7 +2,7 @@ package top.csituka.magicaland;
 
 import net.fabricmc.api.ModInitializer;
 
-public class magicaland implements ModInitializer {
+public class MagicaLand implements ModInitializer {
 
     @Override
     public void onInitialize() {
