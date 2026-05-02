fc45418da4ff47731f6b96c71ed5624483a90f14
JessDaodao <694428809@qq.com>
2026-05-02 16:18:36 +0800

    完善关于页面

--- src/client/java/top/csituka/magicaland/client/gui/tab/About.java
+++ src/client/java/top/csituka/magicaland/client/gui/tab/About.java
@@ -1,12 +1,19 @@
 package top.csituka.magicaland.client.gui.tab;
 
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 import top.csituka.magicaland.client.gui.ConsoleScreen;
 
 public class About implements TabContent {
+
+    private static final Identifier ICON = new Identifier("magicaland", "icon-full.png");
+    private static final String VERSION = FabricLoader.getInstance().getModContainer("magicaland")
+            .map(container -> container.getMetadata().getVersion().getFriendlyString())
+            .orElse("Unknown");
 
     @Override
     public void init(ConsoleScreen screen, int x, int y, int width, int height) {
@@ -16,10 +23,22 @@ public class About implements TabContent {
     @Override
     public void render(DrawContext context, int x, int y, int width, int height, int mouseX, int mouseY, float delta) {
         TextRenderer textRenderer = MinecraftClient.getInstance().textRenderer;
-        int currentY = y + (height - 15) / 2;
+
+        int iconWidth = 128;
+        int iconHeight = 64;
+        int iconX = x + (width - iconWidth) / 2;
+        int iconY = y + (height - iconHeight - 40) / 2;
+
+        context.drawTexture(ICON, iconX, iconY, 0, 0, iconWidth, iconHeight, iconWidth, iconHeight);
 
         Text descriptionText = Text.translatable("text.magicaland.console.about.description");
         int descriptionX = x + (width - textRenderer.getWidth(descriptionText)) / 2;
-        context.drawTextWithShadow(textRenderer, descriptionText, descriptionX, currentY, 0xFFFFFF);
+        int descriptionY = iconY + iconHeight + 10;
+        context.drawTextWithShadow(textRenderer, descriptionText, descriptionX, descriptionY, 0xFFFFFF);
+
+        Text versionText = Text.literal("Version " + VERSION);
+        int versionX = x + (width - textRenderer.getWidth(versionText)) / 2;
+        int versionY = descriptionY + textRenderer.fontHeight + 5;
+        context.drawTextWithShadow(textRenderer, versionText, versionX, versionY, 0xAAAAAA);
     }
 }
